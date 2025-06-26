49311e806be87edbe4e1a0268c154760702fd6cf
mohammed-alnaser <mohammedsameralnaser22@gmail.com>
2025-06-26 23:36:16 +0300

    the first project from react-js

---
+++ Ecommerce/frontend/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+      <div className='descriptionbox'>
+          <div className="desc-navigator">
+              <div className="desc-nav-box">
+                  Description
+              </div>
+               <div className="desc-nav-box fade">
+                  Reviews (122)
+              </div>
+
+          </div>
+          <div className="desc-box-desc">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempora
+                  hic aliquid dignissimos rerum, quia architecto similique. Corrupti, vel ex
+                  saepe animi itaque vero eligendi deserunt numquam optio maiores ipsa?
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempora
+                  hic aliquid dignissimos rerum, quia architecto similique. Corrupti, vel ex
+                  saepe animi itaque vero eligendi deserunt numquam optio maiores ipsa?
+              </p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptatem
+                  qui distinctio non ea et perferendis vitae, nemo neque eveniet provident officia
+                    nesciunt quam natus expedita! Nisi quas saepe voluptas.</p>
+          </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
